fdac507ce1fe2f07c82dcaf905c849c2332b3967
pruthvirajqa2dev <contactpruthviraj@gmail.com>
2025-09-26 16:38:18 +0530

    Update to yml and config for fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 2 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [["blob"], ["json", { outputFile: "test-results.json" }],['html', { outputFolder: 'playwright-report', open: 'never' }]],
+    reporter: [["blob"], ["json", { outputFile: "test-results.json" }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
