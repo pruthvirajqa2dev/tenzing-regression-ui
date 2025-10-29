b76e21978d3a5987ac4b31ed316ab57c0ef66fea
pruthvirajqa2dev <contactpruthviraj@gmail.com>
2025-10-29 13:40:47 +0530

    Add functionality for managing grades, including upload, view, hide, unhide, and delete operations

--- src/tests/Regression/Tenzing.spec.ts
+++ src/tests/Regression/Tenzing.spec.ts
@@ -278,14 +278,14 @@ test.describe("Regression " + `${process.env.test_env}`.toUpperCase(), () => {
             expectedTexts.deleteDialogHeader
         );
         await communityPage.verifyDialogHeaderContent(
-            expectedTexts.deleteDialogContent + inputName + "?"
+            expectedTexts.deletePayScaleDialogContent + inputName + "?"
         );
         await communityPage.click(communityPage.dialogDeleteButtonLocator);
         await expect(
             page.getByText(expectedTexts.payScaleDeletedMsg)
         ).toBeVisible();
     });
-    test.skip("Community - Grades - Add, View, Hide, Unhide, Delete ", async ({
+    test("Community - Grades - Add, View, Hide, Unhide, Delete ", async ({
         page
     }, testInfo) => {
         //Login
@@ -329,203 +329,202 @@ test.describe("Regression " + `${process.env.test_env}`.toUpperCase(), () => {
             expectedTexts.uploadingGradesDialogHeader
         );
 
-        // await communityPage.verifyDialogStartText(
-        //     expectedTexts.mountainBgTextForAddPayScalesDialogPart1,
-        //     expectedTexts.mountainBgTextForAddPayScalesDialogPart2
-        // );
-        // const filePath = path.resolve(paths.payscalesPath);
-        // await page
-        //     .locator(communityPage.fileUploadInputLocator)
-        //     .setInputFiles(filePath);
+        await communityPage.verifyDialogStartText(
+            expectedTexts.mountainBgTextForUploadingGradesDialogPart1,
+            expectedTexts.mountainBgTextForUploadingGradesDialogPart2
+        );
+        const filePath = path.resolve(paths.gradesPath);
+        await page
+            .locator(communityPage.fileUploadInputLocator)
+            .setInputFiles(filePath);
 
-        // const workbook = XLSX.readFile(filePath);
-        // const sheetName = workbook.SheetNames[0];
-        // const sheet = workbook.Sheets[sheetName];
-        // const jsonData = XLSX.utils.sheet_to_json(sheet, {
-        //     header: 1
-        // }) as string[][];
+        const workbook = XLSX.readFile(filePath);
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet, {
+            header: 1
+        }) as string[][];
 
         // //Skip first row (header)
-        // const dataRows = jsonData.slice(1).filter((row) => row.length > 0);
-        // console.log("Data Rows from Excel:", dataRows); // Log data rows
+        const dataRows = jsonData.slice(1).filter((row) => row.length > 0);
+        console.log("Data Rows from Excel:", dataRows); // Log data rows
 
         // //Done: Need to add verification for file uploaded successfully
 
-        // const leftColumnValues = dataRows.map((row) => row[0]);
-
-        // const rightColumnValues = dataRows.map((row) => row[1]);
+        const leftColumnValues = dataRows.map((row) => row[0]);
+        const rightColumnValues = dataRows.map((row) => row[1]);
 
         // //Limit to first 12 values for comparison
-        // const leftColumnSample = leftColumnValues.slice(0, 12);
-        // const rightColumnSample = rightColumnValues.slice(0, 12);
-        // console.log("Left Column Sample:", leftColumnSample);
-        // console.log("Right Column Sample:", rightColumnSample);
+        const leftColumnSample = leftColumnValues.slice(0, 12);
+        const rightColumnSample = rightColumnValues.slice(0, 12);
+        console.log("Left Column Sample:", leftColumnSample);
+        console.log("Right Column Sample:", rightColumnSample);
         // //Logging all left column values from UI grid
-        // const actualLeftColumn = await communityPage.logAllLeftColumnValues();
+        const actualLeftColumn = await communityPage.logAllLeftColumnValues();
         // //Logging all right column values from UI grid
-        // const actualRightColumn = await communityPage.logAllRightColumnValues();
+        const actualRightColumn = await communityPage.logAllRightColumnValues();
 
         // //Remove pound sign from values for comparison
         // // Clean actual UI data
-        // const actualCleaned = actualRightColumn.map((v) =>
-        //     Number(v.replace(/[£,]/g, "").trim())
-        // );
+        const actualCleaned = actualRightColumn.map((v) =>
+            Number(v.replace(/[£,]/g, "").trim())
+        );
 
         // // Ensure same type for both sides
-        // const expectedCleaned = rightColumnSample.map(Number);
+        const expectedCleaned = rightColumnSample.map(Number);
 
-        // console.log("Actual Cleaned:", actualCleaned);
-        // console.log("Expected Cleaned:", expectedCleaned);
+        console.log("Actual Cleaned:", actualCleaned);
+        console.log("Expected Cleaned:", expectedCleaned);
 
-        // expect(actualCleaned.slice(0, 12)).toEqual(
-        //     expectedCleaned.slice(0, 12)
-        // );
+        expect(actualCleaned.slice(0, 12)).toEqual(
+            expectedCleaned.slice(0, 12)
+        );
 
         // //Done: Check the start text at preview upload dialog
-        // await communityPage.verifyDialogStartText(
-        //     expectedTexts.mountainBgTextForPreviewUploadDialogPart1,
-        //     expectedTexts.mountainBgTextForPreviewUploadDialogPart2
-        // );
+        await communityPage.verifyDialogStartText(
+            expectedTexts.mountainBgTextForPreviewUploadGradesDialogPart1,
+            expectedTexts.mountainBgTextForPreviewUploadGradesDialogPart2
+        );
 
         // //Save
-        // console.log("Save");
-        // await communityPage.clickDialogSaveBtn();
-        // await expect(
-        //     page.getByText(expectedTexts.payScaleSuccessMsg)
-        // ).toBeVisible();
+        console.log("Save");
+        await communityPage.clickDialogSaveBtn();
+        await expect(
+            page.getByText(expectedTexts.gradeSuccessMsg)
+        ).toBeVisible();
 
-        // await page.waitForLoadState("load");
+        await page.waitForLoadState("load");
 
         // //Verify tile created with expected header
-        // console.log("Verify tile created with expected header");
+        console.log("Verify tile created with expected header");
 
-        // await communityPage.expectElementToHaveText(
-        //     communityPage.getCardHeaderTitleLocator(inputName),
-        //     inputName
-        // );
-        // await communityPage.expectElementToHaveText(
-        //     communityPage.getCardContentLocator(inputDescr),
-        //     inputDescr
-        // );
+        await communityPage.expectElementToHaveText(
+            communityPage.getCardHeaderTitleLocator(inputName),
+            inputName
+        );
+        await communityPage.expectElementToHaveText(
+            communityPage.getCardContentLocator(inputDescr),
+            inputDescr
+        );
 
-        // //View Pay Scale Points
-        // console.log("View Pay Scale Points");
-        // const addedCard = page
-        //     .locator(communityPage.cardLocator)
-        //     .filter({ hasText: inputName });
-        // await addedCard.locator(communityPage.viewBtnLocator).click();
-        // await communityPage.verifyDialogHeaderText(
-        //     expectedTexts.viewPayScaleDialogHeaderPart + inputName
-        // );
+        // //View Grades
+        console.log("View Grades");
+        const addedCard = page
+            .locator(communityPage.cardLocator)
+            .filter({ hasText: inputName });
+        await addedCard.locator(communityPage.viewBtnLocator).click();
+        await communityPage.verifyDialogHeaderText(
+            expectedTexts.viewGradeDialogHeaderPart + inputName
+        );
 
         // // Close View Dialog
-        // console.log("Close View Dialog");
-        // const viewDialog = page.locator(communityPage.dialogLocator);
-        // await viewDialog
-        //     .locator(communityPage.primaryBtnLocator)
-        //     .filter({ hasText: expectedTexts.closeBtnText })
-        //     .click();
+        console.log("Close View Dialog");
+        const viewDialog = page.locator(communityPage.dialogLocator);
+        await viewDialog
+            .locator(communityPage.primaryBtnLocator)
+            .filter({ hasText: expectedTexts.closeBtnText })
+            .click();
         // //Verify date
-        // const formattedDate = new Date().toLocaleDateString("en-GB");
-        // const expectedDateText = "Date Added: " + formattedDate;
-        // console.log("expectedDateText: " + expectedDateText);
-        // const dateText = await addedCard
-        //     .locator(communityPage.dateLabelLocator)
-        //     .textContent();
-        // console.log("Date Text from UI: " + dateText);
-        // expect(dateText?.trim()).toContain(expectedDateText);
+        const formattedDate = new Date().toLocaleDateString("en-GB");
+        const expectedDateText = "Date Added: " + formattedDate;
+        console.log("expectedDateText: " + expectedDateText);
+        const dateText = await addedCard
+            .locator(communityPage.dateLabelLocator)
+            .textContent();
+        console.log("Date Text from UI: " + dateText);
+        expect(dateText?.trim()).toContain(expectedDateText);
 
         // //Hide Dialog
-        // console.log("Hide Dialog");
-        // await addedCard.locator(communityPage.archiveBtnLocator).click();
+        console.log("Hide Dialog");
+        await addedCard.locator(communityPage.archiveBtnLocator).click();
 
         // //Verify card not visible after archive
-        // console.log("Verify card not visible after archive");
-        // await expect(
-        //     page.locator(communityPage.getCardHeaderTitleLocator(inputName))
-        // ).not.toBeVisible();
-        // await expect(
-        //     page.locator(communityPage.getCardContentLocator(inputDescr))
-        // ).not.toBeVisible();
-        // await expect(
-        //     page.getByText(expectedTexts.payScaleHiddenMsg)
-        // ).toBeVisible();
+        console.log("Verify card not visible after archive");
+        await expect(
+            page.locator(communityPage.getCardHeaderTitleLocator(inputName))
+        ).not.toBeVisible();
+        await expect(
+            page.locator(communityPage.getCardContentLocator(inputDescr))
+        ).not.toBeVisible();
+        await expect(
+            page.getByText(expectedTexts.gradeHiddenMsg)
+        ).toBeVisible();
 
         // //Toggle Show Hidden
-        // console.log("Toggle Show Hidden ON");
-        // await communityPage.toggleShowHidden();
-        // await communityPage.expectElementToHaveText(
-        //     communityPage.getCardHeaderTitleLocator(inputName),
-        //     inputName
-        // );
-        // await communityPage.expectElementToHaveText(
-        //     communityPage.getCardContentLocator(inputDescr),
-        //     inputDescr
-        // );
+        console.log("Toggle Show Hidden ON");
+        await communityPage.toggleShowHidden();
+        await communityPage.expectElementToHaveText(
+            communityPage.getCardHeaderTitleLocator(inputName),
+            inputName
+        );
+        await communityPage.expectElementToHaveText(
+            communityPage.getCardContentLocator(inputDescr),
+            inputDescr
+        );
         // //Unhide Pay Scale
-        // console.log("Unhide Pay Scale");
-        // await addedCard.locator(communityPage.unarchiveBtnLocator).click();
-        // //Verify card not visible after unarchive
-        // console.log("Verify card not visible after unarchive");
-        // await expect(
-        //     page.locator(communityPage.getCardHeaderTitleLocator(inputName))
-        // ).not.toBeVisible();
-        // await expect(
-        //     page.locator(communityPage.getCardContentLocator(inputDescr))
-        // ).not.toBeVisible();
-        // await expect(
-        //     page.getByText(expectedTexts.payScaleUnhiddenMsg)
-        // ).toBeVisible();
+        console.log("Unhide Pay Scale");
+        await addedCard.locator(communityPage.unarchiveBtnLocator).click();
+        //Verify card not visible after unarchive
+        console.log("Verify card not visible after unarchive");
+        await expect(
+            page.locator(communityPage.getCardHeaderTitleLocator(inputName))
+        ).not.toBeVisible();
+        await expect(
+            page.locator(communityPage.getCardContentLocator(inputDescr))
+        ).not.toBeVisible();
+        await expect(
+            page.getByText(expectedTexts.gradeUnhiddenMsg)
+        ).toBeVisible();
         // //Toggle Show Hidden
-        // console.log("Toggle Show Hidden OFF");
-        // await communityPage.toggleShowHidden();
-        // await communityPage.expectElementToHaveText(
-        //     communityPage.getCardHeaderTitleLocator(inputName),
-        //     inputName
-        // );
-        // await communityPage.expectElementToHaveText(
-        //     communityPage.getCardContentLocator(inputDescr),
-        //     inputDescr
-        // );
+        console.log("Toggle Show Hidden OFF");
+        await communityPage.toggleShowHidden();
+        await communityPage.expectElementToHaveText(
+            communityPage.getCardHeaderTitleLocator(inputName),
+            inputName
+        );
+        await communityPage.expectElementToHaveText(
+            communityPage.getCardContentLocator(inputDescr),
+            inputDescr
+        );
 
-        // //Download Pay Scale
-        // await addedCard
-        //     .locator(communityPage.btnLocator, {
-        //         has: page.locator(communityPage.downloadBtnLocator)
-        //     })
-        //     .click();
+        // //Download Grade
+        await addedCard
+            .locator(communityPage.btnLocator, {
+                has: page.locator(communityPage.downloadBtnLocator)
+            })
+            .click();
 
-        // const download = await page.waitForEvent("download");
-        // if (!fs.existsSync("downloads")) fs.mkdirSync("downloads");
+        const download = await page.waitForEvent("download");
+        if (!fs.existsSync("downloads")) fs.mkdirSync("downloads");
 
-        // const downloadedFilePath = `downloads/${await download.suggestedFilename()}`;
-        // await download.saveAs(downloadedFilePath);
+        const downloadedFilePath = `downloads/${await download.suggestedFilename()}`;
+        await download.saveAs(downloadedFilePath);
 
-        // console.log("File saved to:", downloadedFilePath);
-        // var downloadedExcelData: string | null;
-        // if (downloadedFilePath) {
-        //     downloadedExcelData = fs.readFileSync(downloadedFilePath, "utf8");
-        // } else {
-        //     throw new Error("Download path not found.");
-        // }
+        console.log("File saved to:", downloadedFilePath);
+        var downloadedExcelData: string | null;
+        if (downloadedFilePath) {
+            downloadedExcelData = fs.readFileSync(downloadedFilePath, "utf8");
+        } else {
+            throw new Error("Download path not found.");
+        }
 
-        // await communityPage.expectTextNotToBeNull(downloadedExcelData);
-        // console.log("Downloaded Excel Data:", downloadedExcelData);
+        await communityPage.expectTextNotToBeNull(downloadedExcelData);
+        console.log("Downloaded Excel Data:", downloadedExcelData);
 
-        // //Delete Pay Scale
-        // console.log("Delete Pay Scale");
-        // await addedCard.locator(communityPage.deleteBtnLocator).click();
+        // //Delete Grade
+        console.log("Delete Pay Scale");
+        await addedCard.locator(communityPage.deleteBtnLocator).click();
         // //Confirm Delete
-        // console.log("Confirm Delete");
-        // await communityPage.verifyDialogHeaderText(
-        //     expectedTexts.deleteDialogHeader
-        // );
-        // await communityPage.verifyDialogHeaderContent(
-        //     expectedTexts.deleteDialogContent + inputName + "?"
-        // );
-        // await communityPage.click(communityPage.dialogDeleteButtonLocator);
-        // await expect(
-        //     page.getByText(expectedTexts.payScaleDeletedMsg)
-        // ).toBeVisible();
+        console.log("Confirm Delete");
+        await communityPage.verifyDialogHeaderText(
+            expectedTexts.deleteDialogHeader
+        );
+        await communityPage.verifyDialogHeaderContent(
+            expectedTexts.deleteGradeDialogContent + inputName + "?"
+        );
+        await communityPage.click(communityPage.dialogDeleteButtonLocator);
+        await expect(
+            page.getByText(expectedTexts.gradeDeletedMsg)
+        ).toBeVisible();
     });
 });
